9cb8100bdfa818f6aef88605896654e9de3ae995
AzizbekPersonal <tomasshelby779@gmail.com>
2024-11-15 00:53:50 +0500

    "Updated package-lock.json, package.json, and added/modified src/components/Nav.jsx and src/sections/Hero.jsx files"

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,11 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container">
+        
+      </section>
+  );
 };
 
 export default Hero;
